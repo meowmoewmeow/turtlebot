4f79a31e8353bc937297060f7f486b697abdfac6
HYUKGYU <chunhyukgyu@gmail.com>
2025-12-16 22:20:51 +0900

    edit front angle & oscillation

--- self_drive.py
+++ self_drive.py
@@ -8,23 +8,20 @@
 #include <limits>
 
 #include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
-#include <nav_msgs/msg/odometry.hpp>
 
 using namespace std::chrono_literals;
 
 class SelfDrive : public rclcpp::Node
 {
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pose_pub_;
+  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr pose_pub_;
   int step_;
 
-  const float ROBOT_WIDTH = 0.25f;
-  const float CORRIDOR_WIDTH = 0.4f;
   const float TARGET_SPEED = 0.15f;
   const float OBSTACLE_DISTANCE = 0.35f;
-  const float TURN_SPEED = 0.5f;
+  const float TURN_SPEED = 0.3f;
 
 public:
   SelfDrive() : rclcpp::Node("self_drive"), step_(0)
@@ -35,17 +32,21 @@ public:
     scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", lidar_qos_profile, callback);
     
     auto vel_qos_profile = rclcpp::QoS(rclcpp::KeepLast(1));
-    pose_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", vel_qos_profile);
+    pose_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/cmd_vel", vel_qos_profile);
   }
 
   void subscribe_scan(const sensor_msgs::msg::LaserScan::SharedPtr scan)
   {
-    float front_dist = get_scan_min(scan, 0, 10) < get_scan_min(scan, 350, 360) 
-                       ? get_scan_min(scan, 0, 10) : get_scan_min(scan, 350, 360);
-    float left_dist = get_scan_min(scan, 80, 100);
-    float right_dist = get_scan_min(scan, 260, 280);
+    float front_dist = get_scan_min(scan, 0, 2);
+    float front_left = get_scan_min(scan, 358, 360);
+    front_dist = std::min(front_dist, front_left);
 
-    geometry_msgs::msg::Twist vel;
+    float left_dist = get_scan_min(scan, 85, 95);
+    float right_dist = get_scan_min(scan, 265, 275);
+
+    geometry_msgs::msg::TwistStamped vel;
+    vel.header.stamp = this->now();
+    vel.header.frame_id = "base_link";
 
     if (front_dist < OBSTACLE_DISTANCE)
     {
@@ -80,35 +81,31 @@ private:
     return min_dist;
   }
 
-  void handle_corner(geometry_msgs::msg::Twist &vel, float left_dist, float right_dist)
+  void handle_corner(geometry_msgs::msg::TwistStamped &vel, float left_dist, float right_dist)
   {
-    vel.linear.x = 0.0;
+    vel.twist.linear.x = 0.0;
     
     if (left_dist > right_dist)
-    {
-      vel.angular.z = TURN_SPEED;
-    }
+      vel.twist.angular.z = TURN_SPEED;
     else
-    {
-      vel.angular.z = -TURN_SPEED;
-    }
+      vel.twist.angular.z = -TURN_SPEED;
   }
 
-  void follow_lane(geometry_msgs::msg::Twist &vel, float left_dist, float right_dist)
+  void follow_lane(geometry_msgs::msg::TwistStamped &vel, float left_dist, float right_dist)
   {
-    vel.linear.x = TARGET_SPEED;
-
-    float error = left_dist - right_dist;
-    float kp = 1.5;
+    vel.twist.linear.x = TARGET_SPEED;
 
     if (std::isinf(left_dist)) left_dist = 2.0;
     if (std::isinf(right_dist)) right_dist = 2.0;
 
-    vel.angular.z = kp * error;
+    float error = left_dist - right_dist;
+    float kp = 0.6; 
+
+    vel.twist.angular.z = kp * error;
     
-    float max_ang = 1.0;
-    if (vel.angular.z > max_ang) vel.angular.z = max_ang;
-    if (vel.angular.z < -max_ang) vel.angular.z = -max_ang;
+    float max_ang = 0.8;
+    if (vel.twist.angular.z > max_ang) vel.twist.angular.z = max_ang;
+    if (vel.twist.angular.z < -max_ang) vel.twist.angular.z = -max_ang;
   }
 };
 
